d886d6e71eda0d8089d2cdf8a3591b706addf78d
Color-Kat <katik7979@gmail.com>
2024-10-13 10:55:24 +0300

    Init next app, connect LiveBlocks

---
+++ app/Room.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+    LiveblocksProvider,
+    RoomProvider,
+    ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+    const apiKey = process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY!;
+
+    console.log(apiKey);
+
+    return (
+        <LiveblocksProvider publicApiKey={apiKey}>
+            <RoomProvider id="my-room">
+                <ClientSideSuspense fallback={<div>Loading…</div>}>
+                    {children}
+                </ClientSideSuspense>
+            </RoomProvider>
+        </LiveblocksProvider>
+    );
+}
